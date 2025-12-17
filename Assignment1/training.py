fbbbeae08d7285e68e7eba2726f7ff283c0e3002
Aditya Saxena <137895529+adityaaa2511@users.noreply.github.com>
2025-12-17 17:07:43 +0530

    Added code for decoder

--- Assignment1/training.py
+++ Assignment1/training.py
@@ -14,7 +14,7 @@ def scaled_dot_product_attention(q,k,v,mask=None):
     return output, attn
 
 class MultiHeadAttention(nn.Module):
-    def __init__(self,d_model,num_heads=8):
+    def __init__(self,d_model,num_heads=8,dropout=0.1):
         super().__init__()
         assert d_model % num_heads == 0 
         self.d_model = d_model
@@ -22,7 +22,7 @@ class MultiHeadAttention(nn.Module):
         self.d_k = d_model // num_heads
         self.qkv = nn.Linear(d_model, d_model * 3)
         self.o_proj = nn.Linear(d_model, d_model)
-        self.dropout = nn.Dropout(0.1)
+        self.dropout = nn.Dropout(dropout)
 
     def _split_heads(self,x):
         B,L,_ = x.shape
@@ -47,4 +47,48 @@ class MultiHeadAttention(nn.Module):
         attn_out = self._merge_heads(attn_out)
         out = self.o_proj(attn_out)
         out = self.dropout(out)
-        return out, attn
+        return out
+    
+
+class FeedForward(nn.Module):
+    def __init__(self,d_model,d_ff,dropout=0.1):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(d_model,d_ff),
+            nn.ReLU(),
+            nn.Dropout(dropout),
+            nn.Linear(d_ff,d_model),
+            nn.Dropout(dropout)
+        )
+
+    def forward(self,x):
+        return self.net(x)
+    
+class TransformerBlock(nn.Module):
+    def __init__(self,d_model,d_ff,num_heads,dropout=0.1):
+        super().__init__()
+        self.mha = MultiHeadAttention(d_model,num_heads,dropout)
+        self.ffn = FeedForward(d_model,d_ff,dropout)
+        self.ln1 = nn.LayerNorm(d_model)
+        self.ln2 = nn.LayerNorm(d_model)
+
+    def forward(self,x,mask=None):
+        x = x + self.mha(self.ln1(x),mask)
+        x = x + self.ffn(self.ln2(x))
+        return x
+    
+class Decoder(nn.Module):
+    def __init__(self,vocab_size,d_model,d_ff,num_heads=8,num_layers=3,dropout=0.1):
+        super().__init__()
+        self.embedding = nn.Embedding(vocab_size,d_model)
+        self.layers = nn.ModuleList([TransformerBlock(d_model,d_ff,num_heads,dropout) for _ in range(num_layers)])
+        self.ln = nn.LayerNorm(d_model)
+        self.output_proj = nn.Linear(d_model,vocab_size, bias=False)
+
+    def forward(self,x,mask=None):
+        x = self.embedding(x)
+        for layer in self.layers:
+            x = layer(x,mask)
+        x = self.ln(x)
+        logits = self.output_proj(x)
+        return F.softmax(logits,dim=-1)
